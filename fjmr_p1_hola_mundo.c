d6b00c8252fa128c0cc0305a2f93d6050a8dfb95
felipemiramontesr <felipemiramontesr@gmail.com>
2016-07-18 14:48:06 -0500

    Hola mundo en lenguage C

---
+++ fjmr_p1_hola_mundo.c
@@ -0,0 +1,17 @@
+/* Descripción del código: 
+ * programa que realiza la impresión
+ * de la frase "¡Hola mundo!" en terminal.
+ *
+ * Codificador: Mtro. Felipe de Jesús Miramontes Romero
+ * E-mail: felipemiramontesr@gmail.com
+ * Fecha: dom 17 jul 2016 20:38
+ */
+ 
+/*Paquetes utilizados.*/
+#include <stdio.h>
+
+/*Función autoejecutable.*/
+int main(){
+
+	/*Impresión del mensaje.*/
+	printf("¡Hola Felipe! Este es tu primer programa desarrollado con lenguaje C.\n");}
